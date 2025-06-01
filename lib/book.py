6632a501601f496eaa48169da37acf9e2b61ff31
yusufmim <yusufmim2001@gmail.com>
2025-06-01 21:24:21 +0300

    commit v3

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, author, page_count, genre):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.genre = genre
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def get_genre(self):
+        return self._genre
+
+    def set_genre(self, value):
+        self._genre = value
+
+    genre = property(get_genre, set_genre)
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color, material):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.material = material
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        if isinstance(value, (int, float)):
+            self._size = value
+        else:
+            print("size must be a number")
+
+    size = property(get_size, set_size)
+
+    def get_material(self):
+        return self._material
+
+    def set_material(self, value):
+        self._material = value
+
+    material = property(get_material, set_material)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")

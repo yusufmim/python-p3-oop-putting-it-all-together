07363bf7e412ef577d56d93bb773a545927b4e0f
yusufmim <yusufmim2001@gmail.com>
2025-06-01 21:40:32 +0300

    commit 5

--- lib/book.py
+++ lib/book.py
@@ -2,40 +2,40 @@
 
 class Book:
     def __init__(self, title):
-        self.title = title  # uses the property setter
-        self._page_count = 0
-        self._genre = None
+        self.title = title
+        self.page_count = 0
+        self.genre = None
 
     def get_title(self):
         return self._title
 
-    def set_title(self, title):
-        if isinstance(title, str):
-            self._title = title
+    def set_title(self, value):
+        if isinstance(value, str):
+            self._title = value
         else:
-            print("Title must be a string")
+            raise TypeError("Title must be a string")
 
     title = property(get_title, set_title)
 
     def get_page_count(self):
         return self._page_count
 
-    def set_page_count(self, count):
-        if isinstance(count, int):
-            self._page_count = count
+    def set_page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
         else:
-            print("page_count must be an integer")
+            raise TypeError("page_count must be an integer")
 
     page_count = property(get_page_count, set_page_count)
 
     def get_genre(self):
         return self._genre
 
-    def set_genre(self, genre):
-        if isinstance(genre, str):
-            self._genre = genre
+    def set_genre(self, value):
+        if isinstance(value, str):
+            self._genre = value
         else:
-            print("genre must be a string")
+            raise TypeError("genre must be a string")
 
     genre = property(get_genre, set_genre)
 
--- lib/shoe.py
+++ lib/shoe.py
@@ -3,63 +3,63 @@
 class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
-        self.size = size  # uses the property setter
-        self._color = None
-        self._material = None
-        self._condition = "New"
+        self.size = size
+        self.color = None
+        self.material = None
+        self.condition = "New"
 
     def get_brand(self):
         return self._brand
 
-    def set_brand(self, brand):
-        if isinstance(brand, str):
-            self._brand = brand
+    def set_brand(self, value):
+        if isinstance(value, str):
+            self._brand = value
         else:
-            print("Brand must be a string")
+            raise TypeError("Brand must be a string")
 
     brand = property(get_brand, set_brand)
 
     def get_size(self):
         return self._size
 
-    def set_size(self, size):
-        if isinstance(size, (int, float)):
-            self._size = size
+    def set_size(self, value):
+        if isinstance(value, (int, float)):
+            self._size = value
         else:
-            print("size must be a number")
+            raise TypeError("size must be a number")
 
     size = property(get_size, set_size)
 
     def get_color(self):
         return self._color
 
-    def set_color(self, color):
-        if isinstance(color, str):
-            self._color = color
+    def set_color(self, value):
+        if isinstance(value, str):
+            self._color = value
         else:
-            print("color must be a string")
+            raise TypeError("color must be a string")
 
     color = property(get_color, set_color)
 
     def get_material(self):
         return self._material
 
-    def set_material(self, material):
-        if isinstance(material, str):
-            self._material = material
+    def set_material(self, value):
+        if isinstance(value, str):
+            self._material = value
         else:
-            print("material must be a string")
+            raise TypeError("material must be a string")
 
     material = property(get_material, set_material)
 
     def get_condition(self):
         return self._condition
 
-    def set_condition(self, condition):
-        if isinstance(condition, str):
-            self._condition = condition
+    def set_condition(self, value):
+        if isinstance(value, str):
+            self._condition = value
         else:
-            print("condition must be a string")
+            raise TypeError("condition must be a string")
 
     condition = property(get_condition, set_condition)
 
